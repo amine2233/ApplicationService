45969ca0f5f7405563d9ad8b62266af3a2cd5cb4
BENSALA <externe.amine.bensalah@allianz.fr>
2018-10-19 16:19:14 +0200

    Initial Commit

---
+++ ApplicationService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplicationService
+//
+//  Created by BENSALA on 19/10/2018.
+//  Copyright © 2018 BENSALA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
